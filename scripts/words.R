ae5f2b9a0753dfacdf9bcfe436cb8ce07f0f652c
orchid00 <orchid00@gmail.com>
2018-08-31 14:43:13 +0200

    delete all tests from words

--- scripts/words.R
+++ scripts/words.R
@@ -40,7 +40,7 @@ getPageWords <- function(page_content, nam){
         ungroup()
     
     write_csv(words_counts, paste0("results/words/", nam, ".csv"))
-    #return(words_counts)
+    return(words_counts)
 }
 
 
@@ -69,77 +69,3 @@ plotWords <- function(words_counts, nam){
 # getPageWords(page_content, "1")
 # plotWords(mywords, "1")
 
-
-
-
-
-# using xpath
-#job_url %>% 
-#    html_nodes(xpath = '/html/body/div[1]/div[3]/div[3]/div/div/div[1]/div[1]') %>% 
-#    html_text()
-
-
-# job_url <- "https://www.indeed.com/rc/clk?jk=788e7e311656fc54&fccid=09fad757f3449fa5&vjs=3"
-# 
-# tryCatch({
-#     html <- read_html(job_url)
-#     text <- html_text(html)
-#     text <- clean.text(text)
-#     df <- data.frame(skill = KEYWORDS, count = ifelse(str_detect(text, KEYWORDS), 1, 0))
-#     res$running$count <- res$running$count + df$count
-#     res$num_jobs <- res$num_jobs + 1
-# }, error = function(e) {
-#     cat("ERROR :", conditionMessage(e), "\n")
-# })
-
-
-
-
-
-# test1 -------------------------------------------------------------------
-
-
-# require(tm)
-# # convert list into corpus 
-# mycorpus <- Corpus(VectorSource(pages))
-# # prepare to remove stopwords, ie. common words like 'the'
-# skipWords <- function(x) removeWords(x, stopwords("english"))
-# # prepare to remove other bits we usually don't care about
-# funcs <- list(tolower, removePunctuation, removeNumbers, stripWhitespace, skipWords)
-# # do it
-# a <- tm_map(mycorpus, FUN = tm_reduce, tmFuns = funcs)
-# # make document term matrix
-# mydtm <- DocumentTermMatrix(a, control = list(wordLengths = c(3,10)))
-# 
-# 
-# inspect(mydtm)
-# # you can assign it to a data frame for more convenient viewing
-# my_df <- inspect(mydtm)
-# my_df
-# 
-# sort(apply(mydtm, 2, sum))
-# 
-# 
-
-# content <- 
-# pages %>% 
-#     map_df(~(.) %>% as_tibble) 
-# content
-# colnames(content) <-  "text"
-# 
-# content
-# 
-# content %>%
-#     unnest_tokens(word, text) %>% 
-#     anti_join(stop_words) %>% 
-#     group_by(word) %>%
-#     count(sort = TRUE) %>%
-#     ungroup() %>%
-#     top_n(10) %>%
-#     ggplot(aes(x = fct_reorder(word, n), y = n)) +
-#     geom_bar(stat = "identity", width = 0.5) + 
-#     xlab(NULL) +
-#     coord_flip() +
-#     ylab("Word Frequency") +
-#     ggtitle("Most Common Corpus Words") +
-#     theme(legend.position = "none")
