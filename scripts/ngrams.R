0c35695c756f42666d9d920d4ab1e67b86ef0807
orchid00 <orchid00@gmail.com>
2018-09-07 16:03:26 +0200

    add lolipop plot

--- scripts/ngrams.R
+++ scripts/ngrams.R
@@ -60,7 +60,7 @@ jt <- "Data steward"
 loc <- "United States"
 source("scripts/00_theme.R")
 
-# Visualization
+# Visualisation
 mybigrams %>% head(n = 40) %>% 
     mutate(skill = fct_reorder(bigram, n)) %>% # -n
     ggplot(aes(x = skill, y = n)) + 
@@ -73,3 +73,21 @@ mybigrams %>% head(n = 40) %>%
     theme_Publication() +
     theme(axis.text.x = element_text()) # angle = 90,hjust = 1, vjust = 0.5
 
+
+# # Visualisation 2
+mybigrams %>% head(n = 20) %>% 
+    mutate(skill = fct_reorder(bigram, n),
+           n = n * 100) %>% # -n
+    ggplot(aes(x = skill, y = n)) + 
+    geom_segment( aes(x = skill, xend = skill, y = 0, yend = n),
+                  color = "skyblue") +
+    geom_point( size = 4, colour = "chocolate2") +
+    geom_text(aes(label = paste0(n, "%")), size = 3, vjust = 0, nudge_y = 2, nudge_x = -0.1) +
+    theme_light() +
+    coord_flip() +
+    theme( panel.grid.major.y = element_blank(),
+           panel.grid.minor  = element_blank(),
+           panel.border = element_blank()
+    ) +
+    labs(x = "Skill", y = "Occurrences (%)", 
+         title = paste0("Skill occurrences(%) for ", jt, " in ", loc))
